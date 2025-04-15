d9859258c5c19a59113c5489f42afbb28568d4a6
Justissh14 <justissholden14@gmail.com>
2025-04-15 18:09:04 -0400

    added some custom things to window ui

--- main.lua
+++ main.lua
@@ -7,6 +7,21 @@ end
 
 print("Hello World!");
 
+function love.load()
+    -- ThiS the name of window
+    love.window.setTitle("Prototype-Game!")
+    love.window.setMode(800, 600, {resizable=true, vsync=true})
+    love.graphics.setBackgroundColor(0, 0, 200)
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+    love.graphics.print("THis is a test!", 350, 300)
+    love.graphics.setColor(150, 0, 255, 255)
+    print("Text printed successfully!")
+end
 
 
 
