b6b8c133e30061552ea9723ca85fc6a5e65a3d04
AvariceChimera <joshc725@gmail.com>
2025-05-06 21:32:40 -0400

    working on shooting buttons

--- bullet.lua
+++ bullet.lua
@@ -1,10 +1,39 @@
-require("Classic")
-Bullet = Object:extend()
+Object = require("Classic")
+bullet = Object:extend()
 
-function Bullet:new()
-    self.image = love.graphics.newImage("bullet.png")
+function bullet:new()
+    self.image = love.graphics.newImage("Assets/Images/NL2D_bullet.png")
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+    self.speed = 500
+
 end
 
-function Bullet:draw()
+function bullet:update(dt)
+    if love.keyboard.isDown("up") then bullet.y = bullet.y - bullet.speed * dt end
+    
+    if love.keyboard.isDown("up") and love.keyboard.isDown("left")  then
+        
+    end
+    if love.keyboard.isDown("up") and love.keyboard.isDown("right") then 
+
+
+    end
+    if love.keyboard.isDown("down") then bullet.y = bullet.y + bullet.speed * dt end
+
+    if love.keyboard.isDown("down") and love.keyboard.isDown("left") then
+    
+    end
+
+    if love.keyboard.isDown("down") and love.keyboard.isDown("right") then
+    
+    end
+    
+    if love.keyboard.isDown("left") then bullet.x = bullet.x - bullet.speed * dt end
+
+    if love.keyboard.isDown("right") then bullet.x = bullet.x + bullet.speed * dt end
+end
+
+function bullet:draw()
     love.graphics.draw(self.image)
 end
